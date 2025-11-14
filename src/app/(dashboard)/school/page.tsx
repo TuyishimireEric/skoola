b97cbed80c787b0e80a4003bced62bc053d4f535
TuyishimireEric <tuyishimireericc@gmail.com>
2025-11-14 11:28:59 +0200

    build

--- src/app/(dashboard)/school/page.tsx
+++ src/app/(dashboard)/school/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import DashboardStats from "@/components/dashboard/DashboardStats";
 import Loading from "@/components/loader/Loading";
 import TopMenu from "@/components/menu/TopMenu";
 import { useClientSession } from "@/hooks/user/useClientSession";
 
 const Dashboard: React.FC = () => {
-  const { isLoading, organizationId } = useClientSession();
+  const { isLoading } = useClientSession();
 
   if (isLoading) {
     return <Loading overlay={true} fullScreen={true} />;
@@ -21,7 +20,7 @@ const Dashboard: React.FC = () => {
       />
 
       <div className="flex-1 overflow-auto">
-        <DashboardStats organizationId={organizationId}/>
+        {/* <DashboardStats organizationId={organizationId}/> */}
       </div>
     </div>
   );
